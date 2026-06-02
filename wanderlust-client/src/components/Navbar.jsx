30bd50515a9891882ed962fda279f02296a06da5
mdshantosharker <mdshantosharker@gmail.com>
2026-06-02 16:03:53 +0600

    first commit

---
+++ wanderlust-client/src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center p-4 container mx-auto">
+      <ul className="flex gap-20">
+        <li>
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link href={"/destinations"}>Destinations</Link>
+        </li>
+        <li>
+          <Link href={"/my-bookings"}>My Booking</Link>
+        </li>
+        <li>
+          <Link href={"/admin"}>Admin</Link>
+        </li>
+      </ul>
+
+      <div>
+        <Image
+          src={"/assets/Wanderlast.png"}
+          alt="Wanderlast"
+          width={200}
+          height={100}
+        />
+      </div>
+
+      <ul className="flex gap-10">
+        <li>
+          <Link href={"/login"}>Login</Link>
+        </li>
+        <li>
+          <Link href={"/signup"}>SignUp</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
