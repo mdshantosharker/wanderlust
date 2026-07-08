04f33939d5771dfea532afec3ee4e91d41a47e69
mdshantosharker <mdshantosharker@gmail.com>
2026-07-08 11:13:55 +0600

    complete

--- wanderlust-client/src/app/destinations/[id]/page.jsx
+++ wanderlust-client/src/app/destinations/[id]/page.jsx
@@ -1,6 +1,8 @@
 import BookingCard from "@/components/BookingCard";
 import DeleteAlert from "@/components/DeleteAlert";
 import { EditModal } from "@/components/EditModal";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 import Image from "next/image";
 import React from "react";
 import { FaRegCalendar } from "react-icons/fa6";
@@ -8,11 +10,22 @@ import { LuMapPin } from "react-icons/lu";
 
 const DetailsPage = async ({ params }) => {
   const { id } = await params;
-  const res = await fetch(`http://localhost:5000/destination/${id}`);
+  // const { token } = await auth.api.getToken({
+  //   headers: await headers(),
+  // });
+  // console.log(token);
+
+  const res = await fetch(`http://localhost:5000/destination/${id}`, {
+    // headers: {
+    //   Authorization: `Bearer ${token}`,
+    // },
+  });
   const destination = await res.json();
 
   const { imageUrl, price, destinationName, duration, country, description } =
     destination;
+  console.log(imageUrl);
+  console.log(destinationName);
   return (
     <div className="max-w-7xl mx-auto px-4 lg:px-6 mb-20">
       <div className="flex items-center gap-3 justify-end mt-6 mb-5">
