ec63c15a13e9fb5f226255e51a234a9adf486eeb
mdshantosharker <mdshantosharker@gmail.com>
2026-07-21 11:29:41 +0600

    added get api

--- wanderlust-server/index.js
+++ wanderlust-server/index.js
@@ -23,6 +23,21 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const db = client.db("wanderlust");
+    const destinationCollection = db.collection("destinations");
+
+    app.get("/destination", async (req, res) => {
+      const result = await destinationCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/destination", async (req, res) => {
+      const destinationData = req.body;
+      const result = await destinationCollection.insertOne(destinationData);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
